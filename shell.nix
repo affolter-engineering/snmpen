118512524f88d3f80ad6fc295845bfd1e81e16e5
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-20 09:11:10 +0200

    Add shell.nix as helper

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  python = pkgs.python313;
+  pythonEnv = python.withPackages (ps: [
+    ps.humanize
+    ps.pysnmp
+    ps.rich
+    ps.pytest
+    ps.pytest-asyncio
+  ]);
+in
+pkgs.mkShell {
+  packages = [ pythonEnv ];
+
+  shellHook = ''
+    export PYTHONPATH="$PWD:$PYTHONPATH"
+  '';
+}
